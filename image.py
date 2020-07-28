cec3bb46d32401ff13137ed68756ae9aa6c687d7
artem <ivashenco45@gmail.com>
2020-07-28 23:23:32 +0300

    [feature] add functionality photo creation

---
+++ image.py
@@ -0,0 +1,22 @@
+from PIL import Image, ImageDraw, ImageFont
+from util import progressbar, percent
+
+
+def create_image(time: str):
+    """
+    Создает картинку с временем из строки полученной из параметра time.
+    """
+    image = Image.new('RGB', (500, 500), color='black')
+    W, H = image.size # ширина, высота фото
+    draw = ImageDraw.Draw(image)
+
+    clock_font = ImageFont.truetype(font='fonts/num.ttf', size=150)
+    progressbar_font = ImageFont.truetype(font='fonts/progressbar.ttf', size=22)
+    percent_font = ImageFont.truetype(font='fonts/num.ttf', size=25)
+
+    wt, ht = draw.textsize(time, font=clock_font)
+    draw.text(((W - wt) / 2, ((H - ht) / 2) - 60), time, font=clock_font, fill='blue') # write main clock in photo
+    draw.text((6, 355), progressbar(time), font=progressbar_font, fill='blue') # write progressbar in photo
+    draw.text((410, 350), percent(time), font=percent_font, fill='blue')# write percent in photo
+
+    image.save("current_time.png")
---
+++ util.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+
+def progressbar(time: str):
+    """
+    Вернет строку в виде прогресс бара из параметра text.
+    За каждый час к текущему времени который берется из параметра text к строке progressbar прибавляется символ █,
+    в ином случает ставится символ ─
+    """
+
+    progressbar = ''
+
+    # количество  часов с начала дня
+    current_hour = int(time.split(':')[0])
+
+    # левый край прогресс бара
+    progressbar += '|'
+
+    for hour in range(1, 25):
+        if hour <= current_hour:
+            progressbar += '█'
+            continue
+        progressbar += '─'
+
+    # правая граница прогресс бара
+    progressbar += '|'
+
+    return progressbar
+
+
+def percent(time: str):
+    """
+    :return количество времени с начала дня в процентах
+    """
+    second = 0
+
+    hour, minutes = time.split(':')
+
+    # количество секунд с начала дня
+    second += (int(hour) * (60 * 60)) + (int(minutes) * 60)
+
+    return str(round(second * 100 / 86400, 2)) + '%'
+
+
+
+
+
